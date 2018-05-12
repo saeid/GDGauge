8c98ed927f45bce3d7bbd281c2317c624c400f12
ssaeidd <saeid.basirnia@gmail.com>
2018-05-12 09:46:47 +0430

    add cocoaframework | add podspec

---
+++ source/GDGauge/GDGauge/GDGauge.h
@@ -0,0 +1,19 @@
+//
+//  GDGauge.h
+//  GDGauge
+//
+//  Created by Saeid Basirnia on 5/12/18.
+//  Copyright © 2018 Saeid Basirnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GDGauge.
+FOUNDATION_EXPORT double GDGaugeVersionNumber;
+
+//! Project version string for GDGauge.
+FOUNDATION_EXPORT const unsigned char GDGaugeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDGauge/PublicHeader.h>
+
+
