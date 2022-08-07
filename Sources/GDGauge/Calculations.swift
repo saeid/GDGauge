a2f4e69f256ecdf3e7f109bfaef47d4488d22b78
Saeid <9967486+saeid@users.noreply.github.com>
2022-08-07 18:32:01 +0200

    Extract calculations to Calculations.swift

---
+++ Sources/GDGauge/Calculations.swift
@@ -0,0 +1,49 @@
+//
+//  Calculations.swift
+//  
+//
+//  Created by Saeid on 2022-08-07.
+//
+
+import Foundation
+
+struct Calculations {
+    let minValue: CGFloat
+    let maxValue: CGFloat
+    let sectionsGapValue: CGFloat
+    let startDegree: CGFloat
+    let endDegree: CGFloat
+
+    var totalSeparationPoints: Int {
+        Int((maxValue - minValue) / sectionsGapValue)
+    }
+
+    var calculatedStartDegree: CGFloat {
+        270.0 - startDegree
+    }
+
+    var calculatedEndDegree: CGFloat {
+        return 270.0 - endDegree + 360
+    }
+
+    func getNewPosition(currentValue: CGFloat) -> CGFloat {
+        guard currentValue < maxValue else {
+            return maxValue
+        }
+        let convertedDegree = currentValue * (360.0 - (calculatedEndDegree - calculatedStartDegree))
+        return calculatedStartDegree - (convertedDegree / maxValue)
+    }
+
+    func calculateDegree(for point: CGFloat) -> CGFloat {
+        guard point != 0 else {
+            return calculatedStartDegree
+        }
+        return point == CGFloat(totalSeparationPoints)
+        ? calculatedEndDegree
+        : calculatedStartDegree - ((360.0 - (calculatedEndDegree - calculatedStartDegree)) / CGFloat(totalSeparationPoints)) * point
+    }
+
+    func degreeToRadian(_ degree: CGFloat) -> CGFloat {
+        CGFloat(degree * CGFloat(.pi / 180.0))
+    }
+}
