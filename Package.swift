51f83f5a7f0f401891a85c2c2b0397962867f101
Saeid <9967486+saeid@users.noreply.github.com>
2022-08-07 17:42:46 +0200

    Cleanup package structure

--- Package.swift
+++ Package.swift
@@ -5,8 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "GDGauge",
+    platforms: [
+        .iOS(.v13),
+        .watchOS(.v5),
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "GDGauge",
             targets: ["GDGauge"]),
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import GDGaugeTests
-
-var tests = [XCTestCaseEntry]()
-tests += GDGaugeTests.allTests()
-XCTMain(tests)
